c0151e7c9c57acb078e1e0b02bf95f4131f20a7f
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-11-07 10:41:59 +0530

    fix user.auth middleware

--- server/middlewares/user.auth.js
+++ server/middlewares/user.auth.js
@@ -12,7 +12,8 @@ const isUserAuth = async (req, res, next) => {
 
         const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET)
         if (tokenDecoded.id) {
-            req.body.userId = tokenDecoded.id
+            req.body = { ...req.body, userId: tokenDecoded.id }
+            console.log(req.body.userId)
         }
         else {
             return res.status(401).json({
