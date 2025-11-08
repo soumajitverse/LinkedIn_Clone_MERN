fec3b50aecc0ff155c86394b059c4e6fa88bb039
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-11-09 00:20:35 +0530

    change origin agian

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {
-        origin: "https://linked-in-clone-mern-git-main-soumajit-majumders-projects.vercel.app",
+        origin: "https://linked-in-clone-mern.vercel.app/",
         credentials: true
     }
 ))
