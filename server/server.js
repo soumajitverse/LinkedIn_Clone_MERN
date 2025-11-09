62c9c60445535f7c39f846d8408d42aa6fffe4c1
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-11-09 07:47:16 +0530

    fix origin issuse

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {
-        origin: "https://linked-in-clone-mern.vercel.app",
+        origin: "*",
         credentials: true
     }
 ))
